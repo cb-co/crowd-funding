d5ea37aa96296cdca7c11326f858963ade1f217f
Robert <cmcorporations.adm@gmail.com>
2022-01-02 21:40:02 -0400

    Final changes -complete contract -add configuration files -add gitignore -initialize npm

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -2,26 +2,62 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract CrowdFunding {
-  bool public isOpen;
-  address payable public owner;
-  uint public goal;
-  uint public totalFunded;
-  uint public funds;
+  enum FundRaisingState { Closed, Opened }
 
-  constructor() {
-    isOpen = true;
-    goal = 1000000;
-    totalFunded = 0;
-    owner = payable(msg.sender);
+  struct Contribution {
+    address contributor;
+    uint amount;
   }
 
-  function fundProject() public payable {
-    owner.transfer(msg.value);
-    funds += msg.value;
+  struct Project {
+    uint id;
+    FundRaisingState state;
+    string name;
+    string description;
+    address payable owner;
+    uint goal;
+    uint totalFunded;
+    uint funds;
   }
 
-  function changeProjectState(bool value) public {
-    isOpen = value;
+  Project[] public projects;
+  mapping( uint => Contribution[]) public contributions;
+
+  event Projectfunded(address editor, uint amount);
+  event ProjectStateChanged(address editor, FundRaisingState newState);
+  event ProjectCreated(uint id, string name, string description, uint goal);
+
+  modifier onlyOwner(uint _id) {
+      require(msg.sender == projects[_id].owner, 'Only owner can change the goal.');
+      _;
+  }
+
+  modifier notOwner(uint _id) {
+    require(msg.sender != projects[_id].owner, 'Owner cannot fund the project.');
+    _;
+  }
+
+  function createProject(uint _id, string memory _name, string memory _description, uint _goal) public {
+    require(_goal > 0, 'The fundraising goal must be larger than 0.');
+    Project memory project = Project(_id, FundRaisingState.Opened, _name, _description, payable(msg.sender), _goal, 0, 0);
+    projects.push(project);
+    emit ProjectCreated(_id, _name, _description, _goal);
+  }
+
+  function fundProject(uint _id) public payable notOwner(_id) {
+    require(projects[_id].state == FundRaisingState.Opened, 'The project is not open for funding');
+    require(msg.value > 0, 'You cant contribute with 0 eth');
+    projects[_id].owner.transfer(msg.value);
+    projects[_id].funds += msg.value;
+
+    contributions[_id].push(Contribution(msg.sender, msg.value));
+    emit Projectfunded(msg.sender, msg.value);
+  }
+
+  function changeProjectState(FundRaisingState value, uint _id) public onlyOwner(_id) {
+    require(value != projects[_id].state, 'You cannot change to the same state.');
+    projects[_id].state = value;
+    emit ProjectStateChanged(msg.sender, value);
   }
 
 }
--- truffle-config.js
+++ truffle-config.js
@@ -18,8 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { env } = require('process');
+require('dotenv').config();
+
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
@@ -43,8 +45,21 @@ module.exports = {
     //
     development: {
       host: '127.0.0.1', // Localhost (default: none)
-      port: 8545, // Standard Ethereum port (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
       network_id: '*', // Any network (default: none)
+    },
+
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          env.MNEMONIC,
+          `https://ropsten.infura.io/v3/${env.PROJECT_ID}`
+        ),
+      network_id: 3, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true,
     },
     // Another network with more advanced options...
     // advanced: {
